197cc97071a60be06db61f89530a30f94009467a
Bishrant Adhikari <adhi_bishrant@hotmail.com>
2019-04-10 14:44:53 -0500

    working map demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EsrimapComponent } from './components/esrimap/esrimap.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'map', component: EsrimapComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/esrimap/esrimap.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { loadModules } from 'esri-loader';
+
+@Component({
+  selector: 'app-esrimap',
+  templateUrl: './esrimap.component.html',
+  styleUrls: ['./esrimap.component.scss']
+})
+export class EsrimapComponent implements OnInit {
+  @ViewChild('mapViewNode') private mapViewEl: ElementRef;
+  constructor() { }
+
+  ngOnInit() {
+    this.setupMap();
+  }
+
+  public async setupMap() {
+    const [MapView, EsriMap] = await loadModules(['esri/views/MapView', 'esri/Map'], {css: true});
+    const map = new EsriMap({
+      basemap: 'streets',
+     
+    });
+    const mapView = new MapView({
+      map: map,
+      container: this.mapViewEl.nativeElement,
+      center: [0.1278, 51.5074],
+          zoom: 10,
+    });
+    console.log(mapView)
+  }
+
+}
